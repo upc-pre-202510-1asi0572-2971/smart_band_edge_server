df7bccbe929f478d75056cde73c7040fde448cb0
Angel Velasquez <pcisavel@upc.edu.pe>
2025-06-09 07:42:57 -0500

    feat(iam): redefine behavior as static methods.

--- iam/infrastructure/repositories.py
+++ iam/infrastructure/repositories.py
@@ -4,20 +4,23 @@ from iam.infrastructure.models import Device as DeviceModel
 from iam.domain.entities import Device
 
 class DeviceRepository:
-    def find_by_id_and_api_key(self, device_id: str, api_key: str) -> Optional[Device]:
+    @staticmethod
+    def find_by_id_and_api_key(device_id: str, api_key: str) -> Optional[Device]:
         try:
             device = DeviceModel.get((DeviceModel.device_id == device_id) & (DeviceModel.api_key == api_key))
             return Device(device.device_id, device.api_key, device.created_at)
         except DeviceModel.get().DoesNotExist:
             return None
 
-def get_or_create_test_device() -> Optional[Device]:
-    from datetime import datetime
-    device, created = DeviceModel.get_or_create(
-        device_id = "smart-band-001",
-        defaults = {
-            'api_key': 'test-api-key-123',
-            'created_at': datetime.now()
-        }
-    )
-    return Device(device.device_id, device.api_key, created)
+
+    @staticmethod
+    def get_or_create_test_device() -> Optional[Device]:
+        from datetime import datetime
+        device, created = DeviceModel.get_or_create(
+            device_id = "smart-band-001",
+            defaults = {
+                'api_key': 'test-api-key-123',
+                'created_at': datetime.now()
+            }
+        )
+        return Device(device.device_id, device.api_key, created)
