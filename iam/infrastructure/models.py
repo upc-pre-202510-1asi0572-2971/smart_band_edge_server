d57c71d07929c84a20a75c693385d1bfac62522c
Angel Velasquez <pcisavel@upc.edu.pe>
2025-06-09 07:40:32 -0500

    refactor: organize code.

--- iam/infrastructure/models.py
+++ iam/infrastructure/models.py
@@ -4,9 +4,9 @@ from shared.infrastructure.database import db
 
 
 class Device(Model):
-    device_id = CharField(primary_key=True)
-    api_key = CharField()
-    created_at = DateTimeField()
+    device_id   = CharField(primary_key=True)
+    api_key     = CharField()
+    created_at  = DateTimeField()
     class Meta:
-        database = db
-        table_name = 'devices'
+        database    = db
+        table_name  = 'devices'
